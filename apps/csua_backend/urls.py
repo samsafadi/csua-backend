721bd872d1e65b7279b8ad8e5d148431a180f1d4
Robert Quitt <robertquitt@gmail.com>
2018-09-17 18:52:25 -0700

    Remove homedirs app

--- apps/csua_backend/urls.py
+++ apps/csua_backend/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^api/v2/', include('fiber.rest_api.urls')),
     url(r'^newuser/', include('apps.newuser.urls')),
     url(r'^computers/', include('apps.tracker.urls')),
-    url(r'^~', include('apps.homedirs.urls')),
     url(r'', include('apps.db_data.urls')),
     url(r'^', include("apps.main_page.urls")),
     url(r'', page),
